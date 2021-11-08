cedb20fe131c9228ea17f3d9785ddb7523b0044b
AlexFordnirs <71636706+AlexFordnirs@users.noreply.github.com>
2021-11-08 11:35:31 +0200

    Добавлено 1 и 2 число

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                Console.WriteLine("КАЛЬКУЛЯТОР");
+
+                Console.WriteLine("Введите первое число:");
+                int nam1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Введите первое число:");
+                int nam2 = Convert.ToInt32(Console.ReadLine());
+               
+          
+        }
+    }
+}
